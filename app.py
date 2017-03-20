5eae6805b1fae4bdf2834b03063c2550520862e6
Aaron Li Davis <A@airbears2-10-142-14-71.airbears2.1918.berkeley.edu>
2017-03-19 19:33:47 -0700

    some files

--- app.py
+++ app.py
@@ -1,22 +1,22 @@
 from flask import Flask 
 import os
-import settings.py
 from boto.dynamodb2.fields import HashKey
 from boto.dynamodb2.table import Table
 from flask import Flask
 from flask.ext.dynamo import Dynamo
 
 app = Flask(__name__)
-app.config.from_pyfile("settings.cfg")
+# app.config.from_pyfile("settings.cfg")
 # os.environ["AWS_ACCESS_KEY_ID"] = AWS_ACCESS_KEY_ID
 # os.environ["AWS_SECRET_KEY"] = AWS_SECRET_KEY
 
 @app.route("/") 
 def hello(): 
-	return "Hello World!" 
+	return render_template('index.html') 
 
-@app.route("/")
+@app.route("/fuck")
 def DB():
+	return "fuck"
 
 
 if __name__ == "__main__": 
