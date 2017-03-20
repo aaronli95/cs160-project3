99725a09953127af8872550144fb40cbda951765
Aaron Li Davis <A@airbears2-10-142-14-71.airbears2.1918.berkeley.edu>
2017-03-19 17:46:37 -0700

    Flask Website Template

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask 
+import os
+import settings.py
+from boto.dynamodb2.fields import HashKey
+from boto.dynamodb2.table import Table
+from flask import Flask
+from flask.ext.dynamo import Dynamo
+
+app = Flask(__name__)
+app.config.from_pyfile("settings.cfg")
+# os.environ["AWS_ACCESS_KEY_ID"] = AWS_ACCESS_KEY_ID
+# os.environ["AWS_SECRET_KEY"] = AWS_SECRET_KEY
+
+@app.route("/") 
+def hello(): 
+	return "Hello World!" 
+
+@app.route("/")
+def DB():
+
+
+if __name__ == "__main__": 
+	app.run(host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 8080)))
