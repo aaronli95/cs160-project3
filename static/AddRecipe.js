ffc417e1ba076b1937765445ae202a487d2ead7e
Eddy Kim <edward.soohwan.kim@gmail.com>
2017-03-21 01:46:28 -0700

    Finished Home Page & Add Recipe Page

--- static/AddRecipe.js
+++ static/AddRecipe.js
@@ -1,34 +1,62 @@
 $(document).ready(function(){	
 
-	//Default so only the first page of the side bar is filled while the others are still transparent until clicked
 	$(function(){
 		$(document).ready(function(){
-			// for (int i = 0; i < 20; i++) {
-			// 	var ingredientRow = $("<tr>").append("hi");
-			// 	var recipeRow =$("<tr>").append("hi");
-			// 	$("#ingredients-table").append(ingredientRow);
-			// 	$("#recipes-table").append(recipeRow);
-			// }
-
-
-
+			var theUrl = "https://h89jpk0yjl.execute-api.us-east-1.amazonaws.com/prod/RecipeUpdate?TableName=Recipes"
 			$("#Cancel").click(function() {
 				window.location.href="../templates/index.html";
 			});
 
 			$("#AddRecipe").click(function() {
-				// $.ajax({
-    //       			url: theUrl,
-    //       			type: 'POST',
-    //       			data: JSON.stringify({"TableName": "Recipes", "Item": {"Directions":"sucking myself off", "Ingredients":"eddy is a faggot", "RecipeName":"carmen sucks nuts"}}),
-    //       		success: function(data) {
-    //         		alert("POSTed successfully");
-    //         		},
-    //         		error: function(xhr, ajaxOptions, thrownError) {
-    //             		alert("Error");
-    //         		}
-    //     		});
-				window.location.href="../templates/index.html";
+				var title = document.getElementById("newTitle");
+				var img = document.getElementById("newImage");
+				var ingredientsList = [];
+				var directionsList = [];
+				var ingString = "ing";
+				var dirString = "dir";
+
+				for (var i = 1; i < 19; i++) {
+					var currentIng = document.getElementById(ingString + i.toString());
+					var currentDir = document.getElementById(dirString + i.toString());
+					ingredientsList.push(currentIng.value);
+					directionsList.push(currentDir.value);
+				}
+
+				var resultIng = "";
+				var resultDir = "";
+				for (var i = 0; i < 18; i++) {
+					if (ingredientsList[i] != "") {
+						resultIng += ingredientsList[i];
+						if (i != 17) {
+							resultIng += "\n";
+						}
+					}
+					if (directionsList[i] != "") {
+						resultDir += directionsList[i];
+						if (i != 17) {
+							resultDir += "\n";
+						}
+					}
+				}
+				if (title.value == "" || img.value == "" || resultDir == "" || resultIng == "") {
+					window.alert("Missing 1 or More Components Necessary for a New Recipe.");
+				} else {
+					$.ajaxSetup({
+						async: false
+					});
+					$.ajax({
+			        	url: theUrl,
+			        	type: 'POST',
+			        	data: JSON.stringify({"TableName": "Recipes", "Item": {"Directions":resultDir, "Ingredients":resultIng, "RecipeName":title.value, "URL":img.value}}),
+			        	success: function(data) {
+			        		alert("POSTed successfully");
+			            },
+			            error: function(xhr, ajaxOptions, thrownError) {
+			                alert("Error");
+			            }
+			        });
+			        window.location.href="../templates/index.html";
+				}
 			});
 		});
 	});
